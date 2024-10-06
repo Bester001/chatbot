e672415d1907a1ddadc58139992925c84eebb718
Bester001 <yuvrajdixit@gmail.com>
2024-10-07 04:37:11 +0530

    project structure updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot",
+    version="0.0.1",
+    author="Bester001",
+    author_email="yuvrajdixit88@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"],
+)
